ee6b6666c96663ee28fa75cca1c60df5103dc0f7
eyrcvb1844 <78856825+eyrcvb1844@users.noreply.github.com>
2021-03-08 21:53:17 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath(' ../scripts/'))
+sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.')), 'scripts')
 
 
 # -- Project information -----------------------------------------------------
